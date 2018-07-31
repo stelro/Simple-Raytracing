212306b5d795001be88110815a771e96b4f8bce7
Ro Stelmach <stelmach.ro@gmail.com>
2018-07-31 15:03:09 +0300

    ray class

---
+++ ray.hpp
@@ -0,0 +1,15 @@
+//
+// Created by stel on 31/7/2018.
+//
+
+#ifndef OFFTRACER_RAY_HPP
+#define OFFTRACER_RAY_HPP
+
+#include "
+
+class ray {
+public:
+    ray() {}
+};
+
+#endif //OFFTRACER_RAY_HPP
