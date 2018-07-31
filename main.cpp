cb36aa6b8bbe8f2247d4a34054af6499f93845a9
Ro Stelmach <stelmach.ro@gmail.com>
2018-07-31 15:14:19 +0300

    Rays, a simple camera, and background

--- main.cpp
+++ main.cpp
@@ -1,25 +1,39 @@
 #include <iostream>
 #include <fstream>
+#include "ray.hpp"
+
+const int width = 600;
+const int height = 300;
+
+vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction);
+    float t = 0.5 * (unit_direction.y() + 1.0f);
+    return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+}
 
 int main()
 {
-    int width = 200;
-    int height = 100;
 
     std::ofstream file("image.ppm");
-
     file << "P3\n" << width << " " << height << "\n255\n";
+
+    vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
+    vec3 horizontal(4.0f, 0.0f, 0.0f);
+    vec3 vertical(0.0f, 2.0f, 0.0f);
+    vec3 origin(0.0f, 0.0f, 0.0f);
 
     for ( int j = height - 1; j >= 0; j-- ) {
         for ( int i = 0; i < width; i++ ) {
 
-            float r = float (i) / float (width);
-            float g = float (j) / float (height);
-            float b = 2.0f;
+            float u = float (i) / float (width);
+            float v = float (j) / float (height);
 
-            int ir = int(255.99 * r);
-            int ig = int(255.99 * g);
-            int ib = int(255.99 * b);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
+            vec3 col = color(r);
+
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
 
             file << ir << " " << ig << " " << ib << "\n";
         }
--- ray.hpp
+++ ray.hpp
@@ -5,11 +5,16 @@
 #ifndef OFFTRACER_RAY_HPP
 #define OFFTRACER_RAY_HPP
 
-#include "
+#include "vec3.hpp"
 
 class ray {
 public:
     ray() {}
+    ray(const vec3& a, const vec3& b) : origin(a), direction(b) { }
+    vec3 point_at_parameter(float t) const { return origin + t*direction; }
+
+    vec3 origin;
+    vec3 direction;
 };
 
 #endif //OFFTRACER_RAY_HPP
