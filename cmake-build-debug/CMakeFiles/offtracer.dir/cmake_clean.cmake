f75c04346ae29d49355bd393754b48241865673a
Ro Stelmach <stelmach.ro@gmail.com>
2018-07-31 14:50:49 +0300

    first init

---
+++ cmake-build-debug/CMakeFiles/offtracer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/offtracer.dir/main.cpp.o"
+  "offtracer.pdb"
+  "offtracer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/offtracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
