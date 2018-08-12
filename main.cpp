b92e4f07b41ce504c35f9101d3a67f0e09e3c593
stelro <stelmach.ro@gmail.com>
2018-08-12 23:19:36 +0300

    added normal shading map to sphere

--- main.cpp
+++ main.cpp
@@ -5,23 +5,30 @@
 const int width = 600;
 const int height = 300;
 
-bool hit_sphere(const vec3& center, float radius, const ray& r) {
+float hit_sphere(const vec3& center, float radius, const ray& r) {
     vec3 oc = r.origin - center;
     float a = dot(r.direction, r.direction);
     float b = 2.0f * dot(oc, r.direction);
     float c = dot(oc, oc) - radius*radius;
 
     float discriminant = b*b - 4 * a * c;
-    return (discriminant > 0);
+
+    if ( discriminant < 0 ) {
+        return -1.0f;
+    } else {
+        return ( -b - sqrt(discriminant)) / ( 2.0f * a);
+    }
 }
 
 vec3 color(const ray& r) {
-    if ( hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r)) {
-        return vec3(1.0f, 0.0f, 0.0f);
+    float t =  hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r);
+    if ( t > 0.0f ) {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0.0f, 0.0f, -1.0f));
+        return 0.5f * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction);
-    float t = 0.5 * (unit_direction.y() + 1.0f);
+    t = 0.5f * (unit_direction.y() + 1.0f);
     return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
 
