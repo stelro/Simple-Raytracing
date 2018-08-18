ba8c53a58cc0f89a5a62a7cd21b1e2bd88cc8646
stelro <stelmach.ro@gmail.com>
2018-08-18 15:30:49 +0300

    testings

--- cmake-build-debug/CMakeFiles/3.12.0/CMakeCXXCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.12.0/CMakeCXXCompiler.cmake
@@ -19,9 +19,9 @@ set(CMAKE_CXX_SIMULATE_VERSION "")
 
 
 set(CMAKE_AR "/usr/bin/ar")
-set(CMAKE_CXX_COMPILER_AR "/usr/bin/gcc-ar-8")
+set(CMAKE_CXX_COMPILER_AR "/usr/bin/gcc-ar-7")
 set(CMAKE_RANLIB "/usr/bin/ranlib")
-set(CMAKE_CXX_COMPILER_RANLIB "/usr/bin/gcc-ranlib-8")
+set(CMAKE_CXX_COMPILER_RANLIB "/usr/bin/gcc-ranlib-7")
 set(CMAKE_LINKER "/usr/bin/ld")
 set(CMAKE_COMPILER_IS_GNUCXX 1)
 set(CMAKE_CXX_COMPILER_LOADED 1)
